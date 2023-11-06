449d355fb1f8414d1b9ef7a19f8aa4666559704d
Abimbola Ikuerowo <ikuerowo13@gmail.com>
2023-11-06 21:23:30 +0100

    Updated

--- photography.js
+++ photography.js
@@ -176,36 +176,6 @@ window.addEventListener('load', function () {
     contentRotator();
     // END CONTENT ROTATOR
    // END ABOUT SECTION 
-
-   // PORTFOLIO
-    const filterItem = document.querySelector(".items");
-    const filterImg = document.querySelectorAll(".gal .image");
-
-    window.onload = ()=>{ 
-      filterItem.onclick = (selectedItem)=>{ 
-        if(selectedItem.target.classList.contains("item")){ 
-          filterItem.querySelector(".active").classList.remove("active"); 
-          selectedItem.target.classList.add("active"); 
-          let filterName = selectedItem.target.getAttribute("data-name");
-          filterImg.forEach((image) => {
-            let filterImges = image.getAttribute("data-name"); 
-            
-            if((filterImges == filterName) || (filterName == "all")){
-              image.classList.remove("hide"); 
-              image.classList.add("show"); 
-            }else{
-              image.classList.add("hide"); 
-              image.classList.remove("show"); 
-            }
-          });
-        }
-      }
-      for (let i = 0; i < filterImg.length; i++) {
-        filterImg[i].setAttribute("onclick", "preview(this)"); 
-      }
-    }
-   // END PORTFOLIO
-
     // FAQS ANSWER SHOW WHEN USE CLICKS THE BUTTON
     var select = document.querySelector(".link1");
     select.addEventListener("click", function (event) {
@@ -387,3 +357,52 @@ window.addEventListener('load', function () {
    // END BODY SECTION 
 });
 })();
+// PORTFOLIO
+const filterItem = document.querySelector(".items");
+const filterImg = document.querySelectorAll(".gal .image");
+
+window.onload = ()=>{ 
+  filterItem.onclick = (selectedItem)=>{ 
+    if(selectedItem.target.classList.contains("item")){ 
+      filterItem.querySelector(".shuffle").classList.remove("shuffle"); 
+      selectedItem.target.classList.add("shuffle"); 
+      let filterName = selectedItem.target.getAttribute("data-name");
+      filterImg.forEach((image) => {
+        let filterImges = image.getAttribute("data-name"); 
+        
+        if((filterImges == filterName) || (filterName == "all")){
+          image.classList.remove("hide"); 
+          image.classList.add("show"); 
+        }else{
+          image.classList.add("hide"); 
+          image.classList.remove("show"); 
+        }
+      });
+    }
+  }
+  for (let i = 0; i < filterImg.length; i++) {
+    filterImg[i].setAttribute("onclick", "preview(this)"); 
+  }
+}
+
+const previewBox = document.querySelector(".preview-box"),
+categoryName = previewBox.querySelector(".title p"),
+previewImg = previewBox.querySelector("img"),
+closeIcon = previewBox.querySelector(".icon-x"),
+shadow = document.querySelector(".shadow");
+
+function preview(element){
+  document.querySelector("body").style.overflow = "hidden";
+  let selectedPrevImg = element.querySelector("img").src; 
+  let selectedImgCategory = element.getAttribute("data-name"); 
+  previewImg.src = selectedPrevImg; 
+  categoryName.textContent = selectedImgCategory; 
+  previewBox.classList.add("show"); 
+  shadow.classList.add("show");
+  closeIcon.onclick = ()=>{ 
+    previewBox.classList.remove("show"); 
+    shadow.classList.remove("show"); 
+    document.querySelector("body").style.overflow = "auto"; 
+  }
+}
+// END PORTFOLIO
